d0a51539df3c1f4ed1b0d4ba1afb9bf33905850b
na2hiro <a@81.la>
2020-10-22 23:51:15 +0900

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,61 @@
+use shogi::{Move, Position, Color};
+use shogi::bitboard::Factory as BBFactory;
+
+fn main() {
+    BBFactory::init();
+    let mut pos = Position::new();
+
+    // Position can be set from the SFEN formatted string.
+    pos.set_sfen("7k1/9/5G1G1/9/9/9/9/9/9 b 2r2b2g4s4n4l18p 1").unwrap();
+
+    let bb = pos.player_bb(Color::Black);
+    for sq in *bb {
+        let piece = pos.piece_at(sq).unwrap();
+        let tos = pos.move_candidates(sq, piece);
+        for to in tos {
+            let mov = Move::Normal {from: sq, to: to, promote: false};
+            pos.make_move(mov).unwrap();
+            if pos.in_check(Color::White) {
+                println!("{}, {}", pos, pos.in_check(Color::Black));
+            }
+            pos.unmake_move().unwrap();
+        }
+    }
+
+    // move_black(&mut pos);
+}
+
+/*
+fn move_black (pos: &mut Position) {
+    let bb = *pos.player_bb(Color::Black);
+    for sq in *bb {
+        let piece = pos.piece_at(sq).unwrap();
+        let tos = pos.move_candidates(sq, piece);
+        for to in tos {
+            let mov = Move::Normal {from: sq, to: to, promote: false};
+            pos.make_move(mov).unwrap();
+            if pos.in_check(Color::White) {
+                println!("{}, {}", pos, pos.in_check(Color::Black));
+                move_white(pos);
+            }
+            pos.unmake_move().unwrap();
+        }
+    }
+}
+
+fn move_white(mut pos: Position) {
+    let bb = pos.player_bb(Color::Black);
+    for sq in *bb {
+        let piece = pos.piece_at(sq).unwrap();
+        let tos = pos.move_candidates(sq, piece);
+        for to in tos {
+            let mov = Move::Normal {from: sq, to: to, promote: false};
+            pos.make_move(mov).unwrap();
+            if pos.in_check(Color::White) {
+                println!("{}, {}", pos, pos.in_check(Color::Black));
+            }
+            pos.unmake_move().unwrap();
+        }
+    }
+}
+*/
