019e3ed0f91b133a652ca790931adec5c8eafbc1
na2hiro <a@81.la>
2020-10-22 23:57:11 +0900

    Move twice

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,10 @@ fn main() {
     // Position can be set from the SFEN formatted string.
     pos.set_sfen("7k1/9/5G1G1/9/9/9/9/9/9 b 2r2b2g4s4n4l18p 1").unwrap();
 
+    move_black(&mut pos);
+}
+
+fn move_black (pos: &mut Position) {
     let bb = pos.player_bb(Color::Black);
     for sq in *bb {
         let piece = pos.piece_at(sq).unwrap();
@@ -16,26 +20,7 @@ fn main() {
             let mov = Move::Normal {from: sq, to: to, promote: false};
             pos.make_move(mov).unwrap();
             if pos.in_check(Color::White) {
-                println!("{}, {}", pos, pos.in_check(Color::Black));
-            }
-            pos.unmake_move().unwrap();
-        }
-    }
-
-    // move_black(&mut pos);
-}
-
-/*
-fn move_black (pos: &mut Position) {
-    let bb = *pos.player_bb(Color::Black);
-    for sq in *bb {
-        let piece = pos.piece_at(sq).unwrap();
-        let tos = pos.move_candidates(sq, piece);
-        for to in tos {
-            let mov = Move::Normal {from: sq, to: to, promote: false};
-            pos.make_move(mov).unwrap();
-            if pos.in_check(Color::White) {
-                println!("{}, {}", pos, pos.in_check(Color::Black));
+                println!("{}", pos);
                 move_white(pos);
             }
             pos.unmake_move().unwrap();
@@ -43,19 +28,20 @@ fn move_black (pos: &mut Position) {
     }
 }
 
-fn move_white(mut pos: Position) {
-    let bb = pos.player_bb(Color::Black);
+fn move_white(pos: &mut Position) {
+    let bb = pos.player_bb(Color::White);
     for sq in *bb {
         let piece = pos.piece_at(sq).unwrap();
         let tos = pos.move_candidates(sq, piece);
         for to in tos {
             let mov = Move::Normal {from: sq, to: to, promote: false};
-            pos.make_move(mov).unwrap();
-            if pos.in_check(Color::White) {
-                println!("{}, {}", pos, pos.in_check(Color::Black));
+            match pos.make_move(mov) {
+                Ok(_) => {
+                    println!("{}", pos);
+                    pos.unmake_move().unwrap();
+                },
+                Err(_) => {}
             }
-            pos.unmake_move().unwrap();
         }
     }
 }
-*/
